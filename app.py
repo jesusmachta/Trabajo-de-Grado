3428e0eb84234ee064f93a716580c480121403ff
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-10-26 13:37:44 -0400

    :bug: arreglamos el AWS y la API que no querian servir, ahora si arrojan los datos que necesitamos

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from botocore.exceptions import BotoCoreError, ClientError
 
 app = Flask(__name__)
 
-rekognition = boto3.client('rekognition')
-
+rekognition = boto3.client('rekognition', region_name='us-west-2')  # Cambia 'us-west-2' por tu región
 @app.route('/analyze-image', methods=['POST'])
 def analyze_image():
     if 'image' not in request.files:
@@ -34,9 +33,5 @@ def analyze_image():
     except (BotoCoreError, ClientError) as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Catalina!'
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=50001)
+    app.run(debug=True, port = 5001)
