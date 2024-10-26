a8654e1ad8415d341b8b91069240cb189587f540
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-10-26 12:28:42 -0400

    :white_check_mark:Conexion a AWS y a la API realizadas

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Catalina!'
+rekognition = boto3.client('rekognition')
+
+@app.route('/analyze-image', methods=['POST'])
+def analyze_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'}), 400
+
+    image = request.files['image'].read()
+
+    try:
+        response = rekognition.detect_faces(
+            Image={'Bytes': image},
+            Attributes=['ALL']
+        )
+
+        # Filtrando los resultados para solo obtener AgeRange, Gender y Emotions
+        filtered_faces = []
+        for face_detail in response['FaceDetails']:
+            filtered_face = {
+                'AgeRange': face_detail.get('AgeRange'),
+                'Gender': face_detail.get('Gender'),
+                'Emotions': face_detail.get('Emotions')
+            }
+            filtered_faces.append(filtered_face)
+
+        return jsonify(filtered_faces)
+
+    except (BotoCoreError, ClientError) as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
