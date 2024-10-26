fde7805f9fb7953fb1ebe8e81d880e34043b3217
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-10-26 12:42:51 -0400

    :white_check_mark: arreglo con render

--- app.py
+++ app.py
@@ -34,5 +34,9 @@ def analyze_image():
     except (BotoCoreError, ClientError) as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/')
+def hello_world():
+    return 'Hello, Catalina!'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=50001)
