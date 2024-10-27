0ce879fe65c772348dd30f3866c6436ae1893cd7
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-10-27 10:19:07 -0400

    :ambulance: revert y volvimos a solo el AWS y el API, ya BD lista ahora solo falta la connexion

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 import os
 from datetime import datetime
-from database import collections
 
 app = Flask(__name__)
 
@@ -15,25 +14,17 @@ def hello_world():
 
 @app.route('/analyze-image', methods=['POST'])
 def analyze_image():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image file provided'}), 400
-    
-    # cambiar después!!!! Es mientras
-    id_camara_input = request.form.get('ID_Camara')
-    tipo_producto_zona_camara = collections.get("Tipo_Producto_Zona_Camara")
-    if id_camara_input:
-        camara_document = tipo_producto_zona_camara.find_one({"ID_Camara": int(id_camara_input)})
-        if not camara_document:
-            return jsonify({'error': f'ID_Camara {id_camara_input} not found in Tipo_Producto_Zona_Camara'}), 404
-        id_camara = camara_document["ID_Camara"]
-    else:
-        id_camara = 1
-
-    fecha = request.form.get('Fecha', datetime.utcnow().strftime('%Y-%m-%d'))  
-    hora = request.form.get('Hora', datetime.utcnow().strftime('%H:%M:%S')) 
-    image = request.files['image'].read()
-
     try:
+        if 'image' not in request.files:
+            app.logger.error("No image file provided")
+            return jsonify({'error': 'No image file provided'}), 400
+
+        image = request.files['image'].read()
+
+        if not image:
+            app.logger.error("Empty image file provided")
+            return jsonify({'error': 'Empty image file provided'}), 400
+
         response = rekognition.detect_faces(
             Image={'Bytes': image},
             Attributes=['ALL']
@@ -48,23 +39,21 @@ def analyze_image():
                 'Emotions': face_detail.get('Emotions')
             }
             filtered_faces.append(filtered_face)
-            collection = collections.get("Persona_AR")
-            document = {
-            "Fecha": fecha,
-            "Hora": hora,
-            "ID_Camara": id_camara,
-            "faces": filtered_faces,
-            "timestamp": datetime.utcnow()
+
+        # Estructurar la respuesta
+        result = {
+            'NumberOfFaces': len(filtered_faces),
+            'Faces': filtered_faces
         }
-        collection.insert_one(document)
-        collection.insert_one(document)
 
-
-        return jsonify(filtered_faces)
-    
+        return jsonify(result)
 
     except (BotoCoreError, ClientError) as e:
+        app.logger.error(f"Error processing image: {e}")
         return jsonify({'error': str(e)}), 500
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
