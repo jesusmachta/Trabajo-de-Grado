f0b3674c846d37051636a9dd152022331a83deff
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-12-16 17:13:34 -0400

    arreglos del puerto

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from backend.routes import initialize_routes
 
@@ -8,4 +9,5 @@ initialize_routes(app)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
